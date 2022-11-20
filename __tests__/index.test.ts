6ccf0be22c1a1904f1fff6ff34450f2b5b8d797c
Serabass <serabass565@gmail.com>
2022-11-20 18:17:24 +0600

    upd

--- __tests__/index.test.ts
+++ __tests__/index.test.ts
@@ -1,21 +1,53 @@
-import fs from "fs";
 import { Parser } from "../src";
 
 describe("test", () => {
-  it("test", async () => {
-    let pa = new Parser(fs.readFileSync('example.go', 'utf8'), {}, {
+  it("test 1", async () => {
+    let pa = new Parser(`
+    [
+      - Добрый <день|вечер>, я хочу заказать :food, :myFn[111] {
+        :food = [
+          - пиццу
+          - суши
+          - пирог
+          - шаверму
+          - бургер :burger {
+            :burger = [
+              - с картошкой
+            ]
+          }
+        ]
+      }
+    ]
+    `, {}, {
       myFn(a: string) {
         return (+a + 1).toString();
       }
     });
 
     let res = await pa.parse();
-    console.log(res);
-
-    return;
     expect(pa).not.toBeNull();
     expect(res).not.toBeNull();
     expect(typeof res).toBe("string");
-    expect(/Добрый (?:день|вечер), я хочу заказать (?:пиццу|суши|пирог|шаверму|бургер с картошкой)/.test(res)).toBeTruthy();
+    expect(/^Добрый (?:день|вечер), я хочу заказать (?:пиццу|суши|пирог|шаверму|бургер с картошкой), 112$/.test(res)).toBeTruthy();
+  });
+
+  it("test 2", async () => {
+    let pa = new Parser(`
+    [
+      - :myFn[111]
+      - :myFn[112]
+    ]
+    `, {}, {
+      myFn(a: string) {
+        return (+a + 1).toString();
+      }
+    });
+
+    let res = await pa.parse();
+    console.dir(res);
+    expect(pa).not.toBeNull();
+    expect(res).not.toBeNull();
+    expect(typeof res).toBe("string");
+    expect(/^11[23]$/.test(res)).toBeTruthy();
   });
 });
