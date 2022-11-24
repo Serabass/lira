7c944bda321e8cc8d624a2f651d16e0277dda114
Serabass <serabass565@gmail.com>
2022-11-24 23:21:05 +0600

    upd

--- src/random.ts
+++ src/random.ts
@@ -4,6 +4,6 @@ import { Random } from "random-js";
 export const random = new Random(); // uses the nativeMath engine
 
 export function pick<T>(source: T[]): T {
-  // let a = random.shuffle(source);
+  let a = random.shuffle(source);
   return random.pick(source);
 }
